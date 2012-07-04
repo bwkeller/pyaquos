a04d3b1d577bc38dc9a9993e206a8638ab35e7da
Ben Keller <malzraa@gmail.com>
2012-07-04 00:45:12 -0400

    Added the html index with web.py templates and updated the webcontrol module.
    It still needs a lot of work, just playing around right now.

--- webcontrol.py
+++ webcontrol.py
@@ -3,10 +3,14 @@ from pyaquos import controller
 import web
 class index:
 	def GET(self):
-		return 'Hello World'
+		i = web.input(command=None, argument=None)
+		print i.command
+		print i.argument
+		return render.index(i.command, i.argument)
 
 if __name__ == "__main__":
 	#aquos = controller('/dev/ttyUSB0')
 	urls = ('/', 'index')
 	app = web.application(urls, globals())
+	render = web.template.render('html/')
 	app.run()
